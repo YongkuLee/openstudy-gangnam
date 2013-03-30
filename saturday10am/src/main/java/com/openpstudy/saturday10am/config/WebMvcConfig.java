6b02f08f7febd6c8ea9c4151f1ddf6190df7c0ff
YongkuLee <yongku84@gmail.com>
2013-03-30 12:42:01 +0900

    modify

--- saturday10am/src/main/java/com/openpstudy/saturday10am/config/WebMvcConfig.java
+++ saturday10am/src/main/java/com/openpstudy/saturday10am/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -11,8 +12,10 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	
-	public WebMvcConfig() {
-		
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/html/**").addResourceLocations("/html/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 	
 	@Bean
